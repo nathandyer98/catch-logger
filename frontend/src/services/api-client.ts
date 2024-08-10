8dd9873b114a2b582a3c9a8fe9231c665911d7e4
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-08-10 17:29:51 +0100

    updated baseUrl in axios

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api",
 })
